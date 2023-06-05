6c3d758ef966f06fb6c9f983aa6cdec6c688e988
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-05 07:45:00 +0000

    gun

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+DigitalOut myled[]={LED1, PB_1};
+int main()
+{
+    while (true) {
+        for(int i=0;i<2;i++){
+            myled[i] = 1;
+        }
+        wait_ms(500);
+        for(int i=0;i<2;i++){
+            myled[i] = 0;
+        }
+        wait_ms(500);
+    }
 }
